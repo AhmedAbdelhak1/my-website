5379fabff73f0b24b070ff56e6ed843278440b42
AhmedAbdelhak1 <ahmedabdelhak0@gmail.com>
2020-09-22 18:44:29 +0200

    additonal changes

--- src/components/Contactme.tsx
+++ src/components/Contactme.tsx
@@ -9,8 +9,8 @@ function Contactme() {
             <h3>If you are interisting in hiring talent and highly motivated learner, let's talk! <h4>Email: ahmedabdelhak0@gmail.com</h4> </h3>.
             
             </div>
-            <a href="https://github.com/AhmedAbdelhak1"><button >Github</button></a>
-            <a href="https://www.linkedin.com/in/ahmed-abdelhak-912598131/"><button>LinkedIn</button></a>
+                <a href="https://github.com/AhmedAbdelhak1"><button >Github</button></a>
+                <a href="https://www.linkedin.com/in/ahmed-abdelhak-912598131/"><button>LinkedIn</button></a>
             
            
         </div>
